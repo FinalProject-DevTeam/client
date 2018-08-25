df94b1e85a0b7b849d826533161c3c3603f526aa
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-08-25 14:37:21 +0700

    finished upload customers data to s3 feature

--- src/views/Learn.vue
+++ src/views/Learn.vue
@@ -8,14 +8,14 @@
         <p class="title">Learn</p>
         <div class="content">
           <div class="buttons">
-            <span class="button is-medium is-success">Upload to S3</span>
+            <span class="button is-medium is-success" @click="uploadToS3()">Upload to S3</span>
           </div>
           <div class="buttons">
-            <span class="button is-medium is-success">Create data Source</span>
-            <span class="button is-medium is-info">Get Status Data</span>
+            <span class="button is-medium is-success" @click="createDataSource()">Create data Source</span>
+            <span class="button is-medium is-info" @click="getDataSourceStatus()">Get Status Data</span>
           </div>
           <div class="buttons">
-            <span class="button is-medium is-success">Create ML Model</span>
+            <span class="button is-medium is-success" @click="createMLModel()">Create ML Model</span>
             <span class="button is-medium is-info">Get Status ML Model</span>
           </div>
           <div class="buttons">
@@ -28,7 +28,6 @@
           </div>
 
           <div class="buttons">
-            <!-- <span class="button is-medium is-success">Create Prediction</span> -->
             <span class="button is-medium is-success">Get Prediction</span>
           </div>
           <div class="buttons">
@@ -51,7 +50,18 @@
         <p class="title">Main column</p>
         <p class="subtitle">With some content</p>
         <div class="content">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</p>
+        <b-field>
+          <b-select placeholder="dataName" v-model="dataName">
+            <option value="customers-data">customers</option>
+            <option value="transactions-data">transactions</option>
+          </b-select>
+        </b-field>
+        <b-field>
+          <b-select placeholder="datasourceId" v-model="datasourceId">
+            <option value="restaurantId">restaurantId</option>
+            <option value="date">date</option>
+          </b-select>
+        </b-field>
         </div>
       </article>
     </div>
@@ -61,13 +71,88 @@
 
 <script>
 import Nav from '../components/Header/Navbar.vue';
-
+import axios from 'axios';
 
 export default {
+  data() {
+    return {
+      dataName: null,
+      datasourceId: null,
+    };
+  },
   components: {
     Nav
   },
+  methods: {
+    async uploadToS3() {
+      let payload;
+      let columns;
+      let id;
+      let folderName;
+      if (this.dataName === 'customers-data') {
+        id = localStorage.getItem('uid');
+        let customersData = await axios.get('http://localhost:3000/customer', {
+            headers: {
+              uid: id,
+            }
+          })
+        folderName = 'customersData';
+        columns = {
+      	  gender: "gender",
+      		birthYear: "birthyear",
+      		occupation: "occupation",
+        }
+        let arrData = customersData.data.data.map((datum) => {
+          return [ datum.genderML , Number(datum.birthYear), datum.occupationML ];
+        })
+        payload = {
+          arrData: arrData,
+          dataName: this.dataName,
+          folderName: folderName,
+          columns: columns,
+          id: id,
+        }
+        let { data } = await axios.post('http://localhost:3000/aws/s3', payload)
+        console.log(data);
+      } else {
 
+      }
+    },
+    createDataSource() {
+      let id;
+      let payload;
+      if (this.datasourceId === 'restaurantId') {
+        id = localStorage.getItem('uid');
+        payload = {
+          dataName: 'customers-data',
+          folderName: 'customersData',
+          id: id,
+        }
+      } else {
+        id = '';
+        payload = {
+          dataName: 'customers-data',
+          folderName: 'customersData',
+          id: id,
+        }
+      }
+      axios.post(`localhost:3000/aws/datasource`, payload)
+    },
+    getDataSourceStatus() {
+      let id;
+      let date = new Date();
+      let today = `${date.getDate()}${date.getMonth()}${date.getFullYear()}`;
+      if (this.datasourceId === 'restaurantId') {
+        id = localStorage.getItem('uid')
+      } else {
+        id = today
+      }
+      axios.get(`http://localhost:3000/aws/datasource/${id}`)
+    },
+    createMLModel() {
+
+    }
+  }
 };
 </script>
 <style>
