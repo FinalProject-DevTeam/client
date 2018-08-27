bbad6d63edcfb016118c55ac77b738290c425145
handipriyono <handimacbook@gmail.com>
2018-08-27 15:24:59 +0700

    dashboard css box

--- src/components/Dashboard/CardLeftDashboard.vue
+++ src/components/Dashboard/CardLeftDashboard.vue
@@ -1,34 +1,34 @@
 <template>
-  <div class="card">
-    <div class="card events-card">
-      <header class="card-header">
-        <p class="card-header-title">
-          {{ name }}
-        </p>
-      </header>
-      <div class="card-table">
-        <div class="content">
-          <table class="table is-fullwidth is-striped">
-            <tbody>
-              <tr v-for="(datum, index) in data" :key="index">
-                <td width="5%"><i class="fa fa-bell-o"></i></td>
-                <td v-if="name === 'Customers'">{{ datum.name }}</td>
-                <td v-else-if="name === 'Transactions'">Ordered : {{ datum.itemsOrdered.toString() }}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+<div class="card">
+  <div class="card events-card">
+    <header class="card-header">
+      <p class="card-header-title">
+        {{ name }}
+      </p>
+    </header>
+    <div class="card-table">
+      <div class="content">
+        <table class="table is-fullwidth is-striped">
+          <tbody>
+            <tr v-for="(datum, index) in data" :key="index">
+              <td width="5%"><i class="fa fa-bell-o"></i></td>
+              <td v-if="name === 'Customers'">{{ datum.name }}</td>
+              <td v-else-if="name === 'Transactions'">Ordered : {{ datum.itemsOrdered.toString() }}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <footer class="card-footer">
-        <a class="card-footer-item"><router-link :to='`/${link}`'>View All</router-link></a>
-      </footer>
     </div>
+    <footer class="card-footer">
+      <a class="card-footer-item">
+        <router-link :to='`/${link}`'>View All</router-link>
+      </a>
+    </footer>
   </div>
+</div>
 </template>
 
 <script>
-
-
 export default {
   props: ['name', 'link', 'data']
 }
